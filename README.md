605d13948bb591a964fb79628937c2a39c5bf261
Maxim <m.al.rybakov@gmail.com>
2025-09-04 11:04:12 +0700

    README file and other

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# iSensors
+# iSensors <img src="https://img.shields.io/badge/R-package-blue" alt="R package" height="24">
 
 **iSensors** is a package for analyzing signaling activity scores of gene panels based on single-cell RNA-seq data stored in Seurat objects. The package allows you to compute signaling activity scores for specified gene panels and store the results as new assays.
 
@@ -13,7 +13,7 @@ install.packages("devtools")
 # Install the iSensor package from main
 devtools::install_github("MironovaLab/iSensors")
 # or from other branch
-devtools::install_github("MironovaLab/iSensors", ref = "iSensors-v1.2.0-dev")
+devtools::install_github("MironovaLab/iSensors", ref = "devel")
 ```
 In case dealing with a mistake like:
 ```
@@ -25,3 +25,35 @@ Run
 usethis::edit_r_environ()
 ```
 and copy your token to the GITHUB_PAT= your token/
+
+## Quick Start
+
+Here's an example of how to load the package, check its version, explore documentation, and run a basic analysis:
+
+```R
+# Load the package
+library(iSensors)
+
+# Check installed version
+packageVersion("iSensors")
+
+# Access general help page
+help("iSensors")
+
+# Load test data (Seurat object)
+testData <- readRDS("testData/testSeurData.rds")
+
+# Load test panel with meta-panels
+testPanel <- LoadSensors(setName = 'testPanelSet', species = 'AT', hormone = 'cyt', customPanels = TRUE,
+                          randomInfo = list('n' = 3, 'sizes' = c(100, 200, 300), majortrend = TRUE),
+                          metaPanels = list(
+                            'meta1' = list('srcPanels' = c("AT_aux_cis_DR5_ARF1", "AT_aux_cistrans_DR5_ARF5_2_up"), rule = mean),
+                            'meta2' = list('srcPanels' = c("AT_aux_cis_DR5_ARF1", "AT_aux_cistrans_DR5_ARF5_2_up"), rule = prod))
+                          )
+
+# Calculate signaling scores for selected panels
+result <- CalcSensors(testData,
+                      seurLayer = "data",
+                      panelSet = testPanel,
+                      signals = c("mean_normed", "median"))
+```
--- scripts/other.R
+++ scripts/other.R
@@ -20,3 +20,73 @@ my_function <- function(method = c("mean", "median")) {
   print(paste("Selected method:", method))
 }
 my_function(c('mean', 'median'))
+
+packageVersion('iSensors')
+
+load_panel_from_rda_all <- function(file_path) {
+  panelEnv <- new.env()
+  load(file_path, envir = panelEnv)
+  panelName <- ls(envir = panelEnv)
+  if (length(panelName) != 1) {
+    warning("Expected one object per panel file, got: ", paste(panelName, collapse = ", "))
+  }
+  panel <- get(panelName, envir = panelEnv)
+  panel$name <- panelName
+  check_iSensorsPanel_obj(panel)
+  return(panel)
+}
+
+pamel_PAT <- load_panel_from_rda_all(paste0(panelsDir_tmp, '/AT_aux_trans_EffluxInflux.rda'))
+length(pamel_PAT)
+View(pamel_PAT[[3]])
+write.table(
+  pamel_PAT[[1]],
+  file = "C:/Users/user/Desktop/ИЦиГ/Sensor proj/PAT_1_slot.txt",
+  sep = "\t",          # Разделитель — табуляция
+  row.names = FALSE,   # Без номеров строк
+  quote = FALSE        # Без кавычек вокруг строк
+)
+
+panelsDir_tmp <- system.file("extdata/geneSensors", package = "iSensors")
+panelsFiles_tmp <- list.files(path = panelsDir_tmp, pattern = '\\.rda$', full.names = FALSE)
+panels_tmp <- load_panel_files(panelsDir = panelsDir_tmp, files = panelsFiles_tmp)
+
+str(panels_tmp)
+
+testPanel <- LoadSensors(setName = 'testPanelSet', species = 'AT',
+                         random = FALSE)
+
+names(testPanel$panels)
+testPanel$panels[['AT_aux_trans_EffluxInflux']]
+length(testPanel$panels[['AT_aux_trans_EffluxInflux']])
+
+library(readxl)
+file_path <- "C:/Users/user/Desktop/ИЦиГ/Sensor proj/2025-08-21_Supplementary_Tables.xlsx"
+df <- read_excel(path = file_path, sheet = "S3", skip = 1)
+View(df)
+colnames(df)
+
+panel_type <- tolower(df[["Panel type"]])
+panel_type[panel_type == "reg"] <- "cistrans"
+panel_name <- gsub("-", "_", df[["Panel name"]])
+panel_name[panel_name == "PAT"] <- "EffluxInflux"
+names_list <- paste(df[["Species"]], "aux", panel_type, panel_name, sep = "_")
+names_list
+df$package_name <- names_list
+df
+df[["Number of genes"]] <- sapply(df[["package_name"]], function(pkg) {
+  # Проверяем, что панель существует в testPanel$panels
+  if (!is.null(testPanel$panels[[pkg]])) {
+    length(testPanel$panels[[pkg]])
+  } else {
+    NA  # Если панели нет, ставим NA
+  }
+})
+df
+write.table(
+  df,
+  file = "C:/Users/user/Desktop/ИЦиГ/Sensor proj/2025-08-21_Supplementary_Tables_S3.txt",
+  sep = "\t",          # Разделитель — табуляция
+  row.names = FALSE,   # Без номеров строк
+  quote = FALSE        # Без кавычек вокруг строк
+)
