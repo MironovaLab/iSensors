fdf9c07f566c669a2d42869309990b14cc1883e4
Maxim <m.al.rybakov@gmail.com>
2026-05-03 12:54:48 +0700

    norm work update

--- R/signal.R
+++ R/signal.R
@@ -74,11 +74,30 @@ iSensor_signal <- function(iSensor_obj, transform = "mean",
   compute_signal <- function(exprData, detectors, method, normed, normBy) {
     if (normed) {
       if (method == "mean") {
-        # print(exprData)
-        exprData <- if (normBy == "cols") exprData / safe_colMeans(exprData) else exprData / rowMeans(exprData)
+        # wrong:
+        # exprData <- if (normBy == "cols") exprData / safe_colMeans(exprData) else exprData / rowMeans(exprData)
+        exprData <- if (normBy == "cols") {
+          sweep(exprData, 2, safe_colMeans(exprData), "/")
+          # cm <- safe_colMeans(exprData)
+          # exprData <- t(t(exprData) / cm)
+        } else {
+          sweep(exprData, 1, rowMeans(exprData), "/")
+          # rm <- rowMeans(exprData)
+          # exprData <- t(t(exprData) / rm)
+        }
       } else if (method == "median") {
         median_func <- function(x) median(x[x != 0], na.rm = TRUE)
-        exprData <- if (normBy == "cols") exprData / apply(exprData, 2, median_func) else exprData / apply(exprData, 1, median_func)
+        # wrong:
+        # exprData <- if (normBy == "cols") exprData / apply(exprData, 2, median_func) else exprData / apply(exprData, 1, median_func)
+        exprData <- if (normBy == "cols") {
+          sweep(exprData, 2, apply(exprData, 2, median_func), "/")
+          # med <- apply(exprData, 2, median_func)
+          # exprData <- t(t(exprData) / med)
+        } else {
+          sweep(exprData, 1, apply(exprData, 1, median_func), "/")
+          # med <- apply(exprData, 1, median_func)
+          # exprData <- t(exprData / med)
+        }
       }
     }
     
